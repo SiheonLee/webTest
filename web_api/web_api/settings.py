a13ee4b50fd5a43e593874626b7be8e799da14cb
SiheonLee <unicon718@naver.com>
2022-01-23 16:20:02 +0100

    add docker, nginx

--- web_api/web_api/settings.py
+++ web_api/web_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y(tthr!6(lz4=l!k4u5#24eq-@_kp8lr_%l=7_$#rli$@&v*y3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
